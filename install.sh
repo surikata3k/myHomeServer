b5a7a5650b5c7cbba4082d0bc4469465eab1adf2
Surikata3k <surikata3k@gmail.com>
2025-05-15 18:05:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
+echo "######## Create folders ########"
 sh 01_createFolders.sh
+
+echo "######## Install docker ########"
 sh 02_docker.sh
+
+echo "######## Run containers ########"
 sh 03_runContainers.sh
+
+echo "######## Set dashboard ########"
 sh 04_dashboard.sh
