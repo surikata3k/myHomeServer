f8580bc589d99dfc580b722a747d0c71f8ec1c81
Surikata3k <surikata3k@gmail.com>
2025-05-15 17:51:35 +0200

    Update 02_docker.sh

--- 02_docker.sh
+++ 02_docker.sh
@@ -1,6 +1,6 @@
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -14,7 +14,7 @@ sudo apt-get -y update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo groupadd docker
+# sudo groupadd docker
 
 sudo usermod -aG docker $USER
 
