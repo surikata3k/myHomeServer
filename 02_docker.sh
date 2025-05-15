75bb6565ab0b0a88f9a81859971ba825efc49448
Surikata3k <surikata3k@gmail.com>
2025-05-15 18:14:03 +0200

    Update 02_docker.sh

--- 02_docker.sh
+++ 02_docker.sh
@@ -14,7 +14,7 @@ sudo apt-get -y update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# sudo groupadd docker
+sudo groupadd docker
 
 sudo usermod -aG docker $USER
 
